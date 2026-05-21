3cbd995aa1b4093f92efaf965819ad5708860673
NovaAbhilok123 <abhilokreddy123@gmail.com>
2026-05-21 16:29:47 +0530

    fix: remove auto-open DevTools on startup

--- electron/main/index.js
+++ electron/main/index.js
@@ -33,7 +33,6 @@ function createMainWindow() {
 
   if (isDev) {
     mainWindow.loadURL('http://localhost:5173');
-    mainWindow.webContents.openDevTools({ mode: 'detach' });
   } else {
     mainWindow.loadFile(path.join(__dirname, '../../renderer/dist/index.html'));
   }
